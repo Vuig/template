4cdadeffcc8357599cd88c13cad5b4b968a7c7f6
Yorick Geerts <y.geerts@gmail.com>
2016-09-26 11:52:51 +0200

    nummer2

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
 	<jdoc:include type="head" />
 	<script src="/media/jui/js/bootstrap.min.js" type="text/javascript"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/jui/css/bootstrap.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/media/jui/css/bootstrap-responsive.css" type="text/css" />
-		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
 </head>
@@ -28,9 +27,9 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</a>
-<a class="brand" href="<?php echo $this->baseurl; ?>">TEST</a>
+			<a class="brand" href="<?php echo $this->baseurl; ?>">TEST</a>
 			<div class="nav-collapse collapse navbar-responsive-collapse">
-<jdoc:include type="modules" name="head" style="none" />
+				<jdoc:include type="modules" name="head" style="none" />
 			</div>
 		</div>
 	</div>
@@ -44,28 +43,33 @@
 </div>
 
 
+
 <div class="container">
 
-
+<!-- CALCULATE WIDTH ARTICLE -->
 
  <?php 
     $columns == 0;
     if($this->countModules( 'right' )) {$columns++;}
     $centercolumnwidth = 100 - (33 * $columns);
     ?>
+
+<!-- ARTICLES -->
+
 <div class="articles" style="width:<?php echo $centercolumnwidth ?>%">
 <jdoc:include type="component"   />
 </div>
 
-
+<!-- SIDEBAR -->
 
 <?php if ($this->countModules( 'right' )) : ?>
-    <div class="sidebar" class="row-fluid">
-        <jdoc:include type="modules" name="right" style="xhtml" />
-    </div><!--/right1-->
+    	<div class="sidebar" class="row-fluid">
+        	<jdoc:include type="modules" name="right" style="xhtml" />
+		</div><!--/right1-->
     <div class="clearfix"></div>
-    <?php endif; ?>
+<?php endif; ?>
 
+<!-- FOOTER -->
 
 <div class="row-fluid">
 <p align="center">&copy; <?php echo(JFactory::getApplication()->getCfg (‘sitename’));?> <?php echo date(‘Y’) ?></p>
@@ -73,9 +77,5 @@
 </div>
 
 
-
-
 </body>
-
-
 </html>
